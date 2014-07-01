bc0f87fb3f75b9e15ba21533ee9d6098ed70197f
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-07-01 14:15:56 +0200

    Use AnyEvent to process the hello packets.

--- hello.pl
+++ hello.pl
@@ -6,53 +6,68 @@ use Fcntl;
 use Data::Dumper;
 use YAML;
 
+use AnyEvent;
+use AnyEvent::Handle;
+use AnyEvent::Strict;
+
 use Packet;
 
-my $tun_device = 6;
+my $tun_device = "/dev/tun6";
 my $mac_address = "1:2:3:4:5:6";
 my $ospf_address = "10.188.6.18";
 my $router_id = "10.188.6.18";
 
-sysopen(my $tun, "/dev/tun$tun_device", O_RDWR)
-    or die "Open /dev/tun$tun_device failed: $!";
+my $cv = AnyEvent->condvar;
 
-for (;;) {
-    my $n = sysread($tun, my $packet, 70000);
-    defined($n) or die "sysread failed: $!";
-    $n or last;
-    print "Read $n bytes\n";
+sysopen(my $tun, $tun_device, O_RDWR)
+    or die "Open $tun_device failed: $!";
 
-    my %ether = consume_ether(\$packet);
-    unless ($ether{type} == 0x0800) {
-	warn "ether type is not ip4";
-	next;
-    }
-    my %ip4 = consume_ip4(\$packet);
-    unless ($ip4{p} == 89) {
-	warn "ip4 proto is not ospf";
-	next;
-    }
-    my %ospf = consume_ospf(\$packet);
-    unless ($ospf{type} == 1) {
-	warn "ospt type is not hello";
-	next;
-    }
-    my %hello = consume_hello(\$packet);
+my $handle; $handle = AnyEvent::Handle->new(
+    fh => $tun,
+    read_size => 70000,  # little more then max ip size
+    on_error => sub {
+	$cv->croak("error on $tun_device: $!");
+	$handle->destroy();
+	undef $handle;
+    },
+    on_eof => sub {
+	$cv->croak("end-of-file on $tun_device: $!");
+	$handle->destroy();
+	undef $handle;
+    },
+    on_read => sub {
+	my %ether = consume_ether(\$handle->{rbuf});
+	unless ($ether{type} == 0x0800) {
+	    warn "ether type is not ip4";
+	    return;
+	}
+	my %ip4 = consume_ip4(\$handle->{rbuf});
+	unless ($ip4{p} == 89) {
+	    warn "ip4 proto is not ospf";
+	    return;
+	}
+	my %ospf = consume_ospf(\$handle->{rbuf});
+	unless ($ospf{type} == 1) {
+	    warn "ospt type is not hello";
+	    return;
+	}
+	my %hello = consume_hello(\$handle->{rbuf});
+	$handle->{rbuf} = "";  # just to be sure, packets must not cumulate
 
-    $ether{src_str} = $mac_address;
-    $ip4{src_str} = $ospf_address;
-    $ospf{router_id_str} = $router_id;
-    $hello{backup_designated_router_str} = $router_id;
-    $hello{neighbors_str} = [ "10.188.6.17" ];
-    $packet = "";
-    $packet .= construct_ether(\%ether);
-    $packet .= construct_ip4(\%ip4,
-	construct_ospf(\%ospf,
-	construct_hello(\%hello)));
+	$ether{src_str} = $mac_address;
+	$ip4{src_str} = $ospf_address;
+	$ospf{router_id_str} = $router_id;
+	$hello{backup_designated_router_str} = $router_id;
+	$hello{neighbors_str} = [ "10.188.6.17" ];
+	$handle->push_write(
+	    construct_ether(\%ether,
+	    construct_ip4(\%ip4,
+	    construct_ospf(\%ospf,
+	    construct_hello(\%hello))))
+	);
+    },
+);
 
-    $n = syswrite($tun, $packet);
-    defined($n) or die "syswrite failed: $!";
-    print "Wrote $n bytes\n";
-}
+$cv->recv;
 
 print "Terminating\n"
