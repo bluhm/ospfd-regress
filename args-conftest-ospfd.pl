574391a789b35556a84b525b1dece0e524e298dd
Florian Riehm <mail@friehm.de>
2014-07-01 12:35:27 +0200

    add Test args-conftest-ospfd.pl It starts ospfd in config test mode (-c)

---
+++ args-conftest-ospfd.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+
+our %args = (
+    ospfd => {
+	configtest => 1,
+	conf => {
+	    global => {
+		'router-id' => '1.2.3.4',
+	    },
+	    areas => {
+		'51.0.0.0' => {
+		    'lo0:127.0.0.1' => {
+			'metric' => '15',
+		    },
+		},
+	    },
+	},
+    },
+);
+
+1;
